1e8153e29d5ac15676ab5f73b15fb00b9c154fc9
store <darmenta117@gmail.com>
2021-06-18 18:12:20 -0500

    Components 1

--- course2/conFusion/src/app/app.module.ts
+++ course2/conFusion/src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 
@@ -9,16 +10,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import 'hammerjs';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+    MatListModule,
     FlexLayoutModule
   ],
   providers: [],
---
+++ course2/conFusion/src/app/shared/dish.ts
@@ -0,0 +1,20 @@
+export class Dish{
+  constructor(){
+      this.id = "";
+      this.name = "";
+      this.image= "";
+      this.category= "";
+      this.label= "";
+      this.price= "";
+      this.description= "";
+      this.featured=false;
+  }
+    id: string;
+    name: string;
+    image: string;
+    category: string;
+    featured: boolean;
+    label: string;
+    price: string;
+    description: string;
+}
